fdce054d3187f49b1a2ea3b0b23a7e28d36d5264
Henry Bucklow <patches@elsie.org.uk>
2023-01-02 22:28:30 +0000

    Added pencil holder model.

---
+++ pen_pencil_holder.py
@@ -0,0 +1,41 @@
+import math
+
+import cadquery as cq
+import gridfinity
+
+block_x = 1
+block_y = 1
+block_height = 6
+
+diameter = 8
+taper = 2.5
+depth = (block_height - 1) * 7 + 6
+taper_pad = depth * math.sin((taper / 180) * math.pi)
+
+spacing = 0.4
+
+clearance= 0.15
+
+block = (
+    cq.Workplane("XY")
+    .gridfinity_block(block_x, block_y, block_height)
+    .gridfinity_block_stack(block_x, block_y))
+
+cutout = (
+    block.faces(">Z")
+    .placeSketch(
+        cq.Sketch().circle(diameter / 2 + clearance + taper_pad))
+    .extrude(-depth, combine=False, taper=taper))
+
+block = (
+    block
+    .rarray(diameter + (clearance + taper_pad) * 2 + spacing, diameter + (clearance + taper_pad) * 2 + spacing, 3, 3)
+    .eachpoint(lambda loc: cutout.val().moved(loc), combine="cut")
+    .gridfinity_block_lip(block_x, block_y, screw_depth=3))
+
+#block = block.faces(">Z").chamfer(0.5)
+
+del cutout
+
+cq.exporters.export(block, "pen_pencil_holder.step")
+cq.exporters.export(block, "pen_pencil_holder.stl")
